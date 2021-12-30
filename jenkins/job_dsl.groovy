55da033798e563ddb32c30b05cd56fdc0bb6a3c4
Paul Beaudet <paul.beaudet@epitech.eu>
2021-12-30 17:03:21 +0100

    feat(jobs): Add link project
    The link project can create jobs based on the given informations, it only works for registered languages

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -4,6 +4,10 @@ folder("Base images") {
     description("Contain all the base images")
 }
 
+folder("Projects") {
+    description("Contains all the jobs created by Link-project")
+}
+
 for (language in languages) {
     freeStyleJob("Base images/" + language) {
         description("Build the base image of " + language)
@@ -19,5 +23,19 @@ for (language in languages) {
             }
             shell("echo \"" + language + " base image built !\"")
         }
+    }
+}
+
+freeStyleJob("Link-project") {
+    customWorkspace("/")
+    description("Link a project to a job that check if there is changes in the repo and if yes build and containerize the project")
+    parameters {
+        stringParam("GITHUB_NAME", null, "GitHub repository owner/repo_name (e.g.: \"Pepiloto/Pushswap\")")
+        stringParam("DISPLAY_NAME", null, "Display name for the job (no spaces in the name)")
+        stringParam("BINARY_NAME", "compiled-app", "Name of the binary to run")
+        stringParam("IMAGE_NAME", "none", "Name of the image that will be built if there is a Dockerfile in the repo (no spaces in the name)")
+    }
+    steps {
+        dsl(["jenkins/template.groovy"])
     }
 }
---
+++ jenkins/template.groovy
@@ -0,0 +1,65 @@
+def languages = [ "c" ]
+
+freeStyleJob("Projects/${DISPLAY_NAME}") {
+    scm {
+        git {
+            branches('main', 'master')
+            extensions {
+                cleanBeforeCheckout()
+                choosingStrategy {
+                    alternative()
+                }
+                cloneOptions {
+                    depth(1)
+                    shallow(true)
+                }
+            }
+            remote {
+                github("${GITHUB_NAME}")
+            }
+        }
+    }
+    triggers {
+        scm("* * * * *")
+    }
+    steps {
+        for (language in languages) {
+            steps {
+                conditionalSteps {
+                    condition {
+                        fileExists("Dockerfile", BaseDir.WORKSPACE)
+                    }
+                    steps {
+                        dockerBuildAndPublish {
+                            dockerfileDirectory("Dockerfile")
+                            dockerRegistryURL("http://localhost:5000")
+                            repositoryName("${IMAGE_NAME}")
+                            tag("${DISPLAY_NAME}")
+                            forcePull(false)
+                            createFingerprints(false)
+                            forceTag(false)
+                        }
+                    }
+                }
+                conditionalSteps {
+                    condition {
+                        not {
+                            fileExists("Dockerfile", BaseDir.WORKSPACE)
+                        }
+                    }
+                    steps {
+                        shell("python3 /jenkins/create_standalone.py " + language + " ${BINARY_NAME}")
+                        dockerBuildAndPublish {
+                            dockerfileDirectory("/images/" + language + "/Dockerfile.standalone")
+                            dockerRegistryURL("http://localhost:5000")
+                            repositoryName("${IMAGE_NAME}")
+                            forcePull(false)
+                            createFingerprints(false)
+                            forceTag(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
